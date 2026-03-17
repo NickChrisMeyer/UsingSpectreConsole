6d97f38c2b42533e679482fe415b9877672f397a
Nicholas Christopher Meyer <101266733+NickChrisMeyer@users.noreply.github.com>
2026-03-17 13:14:40 +0200

    Add project files.

---
+++ UsingSpectreConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UsingSpectreConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
